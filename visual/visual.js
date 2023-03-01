5844d20d46330d86bb77d70e7444f3a037b395dd
Navinate <treycluff@gmail.com>
2023-03-01 16:34:23 -0500

    added basic movement to gestures

--- visual/visual.js
+++ visual/visual.js
@@ -1,29 +1,35 @@
-//const socket = io(); //LOCAL TESTING
-const socket = io("ws://127.0.0.1:3000"); //LIVE SERVER
+//const socket = io("ws://127.0.0.1:3000"); //LOCAL TESTING
+const socket = io(); //LIVE SERVER
 
 const caps = ["ROUND", "SQUARE", "PROJECT"];
 const joins = ["MITER", "BEVEL", "ROUND"];
 let gests = [];
+let t = 0;
 
 function setup() {
-	//SET BACK TO FULL SIZE AFTER TESTING IS DONE
-	createCanvas(windowWidth, windowHeight - 50);
-	frameRate(20);
+	createCanvas(windowWidth, windowHeight);
+	frameRate(60);
 }
 function draw() {
-	background(33);
+	background(22);
 	push();
 	translate(width / 2, height / 2);
+	let index = 0;
 	gests.forEach((g) => {
+		g.update(t, index);
 		g.render();
+		index++;
 	});
 	pop();
+
+	t += 0.001;
 }
 
 socket.on("server to gesture", (color, points) => {
 	console.log("recieved data");
 	gests.push(
 		new Gesture(
+			random(99999),
 			color,
 			random(100) + 20,
 			random(caps),
@@ -33,4 +39,21 @@ socket.on("server to gesture", (color, points) => {
 		)
 	);
 	gests[gests.length - 1].points = [...points];
+});
+
+document.getElementById("add-dot").addEventListener("mousedown", () => {
+	gests.push(
+		new Gesture(
+			random(99999),
+			"#FF0000",
+			random(100) + 20,
+			random(caps),
+			random(joins),
+			random(-width / 2, width / 2),
+			random(-height / 2, height / 2)
+		)
+	);
+	gests[gests.length - 1].addPoint(0, 0);
+	gests[gests.length - 1].addPoint(4, 4);
+	gests[gests.length - 1].addPoint(20, 20);
 });

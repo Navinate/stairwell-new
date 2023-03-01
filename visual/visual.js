c947b42abfedc59d57a15aec32b79ea981c394db
Navinate <treycluff@gmail.com>
2023-03-01 15:26:15 -0500

    updated form css

--- visual/visual.js
+++ visual/visual.js
@@ -6,30 +6,31 @@ const joins = ["MITER", "BEVEL", "ROUND"];
 let gests = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(20);
+	//SET BACK TO FULL SIZE AFTER TESTING IS DONE
+	createCanvas(windowWidth, windowHeight - 50);
+	frameRate(20);
 }
 function draw() {
-  background(33);
-  push();
-  translate(width / 2, height / 2);
-  gests.forEach((g) => {
-    g.render();
-  });
-  pop();
+	background(33);
+	push();
+	translate(width / 2, height / 2);
+	gests.forEach((g) => {
+		g.render();
+	});
+	pop();
 }
 
 socket.on("server to gesture", (color, points) => {
-  console.log("recieved data");
-  gests.push(
-    new Gesture(
-      color,
-      random(100) + 20,
-      random(caps),
-      random(joins),
-      random(-width / 2, width / 2),
-      random(-height / 2, height / 2)
-    )
-  );
-  gests[gests.length - 1].points = [...points];
+	console.log("recieved data");
+	gests.push(
+		new Gesture(
+			color,
+			random(100) + 20,
+			random(caps),
+			random(joins),
+			random(-width / 2, width / 2),
+			random(-height / 2, height / 2)
+		)
+	);
+	gests[gests.length - 1].points = [...points];
 });

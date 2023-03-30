47326e7c51844e897fc63a07e3aadfb6952f9a05
Navinate <treycluff@gmail.com>
2023-03-30 11:24:13 -0400

    added welcome screen pages and kiki img

---
+++ form/welcome.js
@@ -0,0 +1,23 @@
+//Hide all but first page
+let pages = document.querySelectorAll(".page");
+for (let i = 1; i < pages.length; i++) {
+  pages[i].style.display = "none";
+}
+
+let buttons = document.querySelectorAll(".go-next");
+buttons.forEach((b) => {
+  b.addEventListener("click", cyclePages);
+});
+
+let pageIndex = 0;
+function cyclePages() {
+  pages.forEach((p) => {
+    p.style.display = "none";
+  });
+  pageIndex++;
+
+  pages[pageIndex].style.display = "flex";
+  if (pageIndex >= 6) {
+    goNext.style.display = "none";
+  }
+}

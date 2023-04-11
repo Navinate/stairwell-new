e78c3938d5e7309ed3c7dcc6d2236e0fbd64ebc5
Maddy <59680197+xM4ddy@users.noreply.github.com>
2023-04-11 17:12:35 -0400

    Add Settings Page

--- visual/visual.js
+++ visual/visual.js
@@ -1,3 +1,16 @@
+// open child settings window to change options related to the visual
+console.log("http://" + window.location.href.split('/')[2] + "/visual/settings.html");
+if (window.location.href.split('/')[4] != "settings.html") {
+	url = window.location.href.split('/')[1] + "/visual/settings.html"
+	child_window = window.open(url, popup=false);
+	settingsWindow = true;
+}
+
+// variables for bg
+let bg_width, bg_height = 0;
+let bg = "gradient";
+
+
 const socket = connectToWebSocket();
 console.log("connected to websocket");
 
@@ -10,9 +23,14 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	frameRate(60);
 }
+
 function draw() {
 	background(0);
-	setGradient(0, 0, width, height, color(134, 219, 216), color(38, 34, 98));
+
+	bg_width = width;
+	bg_height = height;
+
+	setBackground();
 
 	push();
 	//scale(0.5);
@@ -24,6 +42,25 @@ function draw() {
 	pop();
 
 	t += 0.0005;
+}
+
+// function to change the background
+function setBackground(bg_pram) {
+	if (bg_pram != null) {
+		bg = bg_pram
+	}
+	
+	switch (bg) {
+		case "gradient":
+			setGradient(0, 0, width, height, color(134, 219, 216), color(38, 34, 98));
+			break;
+		case "space":
+			space(width, height, 100, 2);
+			break;
+		default: 
+			//setGradient(0, 0, width, height, color(134, 219, 216), color(38, 34, 98));
+			console.log("Default Background Case. Check bg.")
+	}
 }
 
 socket.on("server to gesture", (points, red, green, blue, alpha, girth, cap, join, speed, wiggle, smoothness) => {
@@ -84,5 +121,36 @@ function setGradient(x, y, w, h, c1, c2) {
 		fill(c);
 		rectMode(CORNERS);
 		rect(x, i, x + w, i + h / 10);
+	}
+}
+
+
+let star_x = [], star_y = [], stars_made = false;
+
+function space(w, h, star_count, star_size) {
+	noStroke();
+	fill(0);
+	rectMode(CORNERS);
+	rect(0, 0, w, h);
+
+	if (stars_made == false) {
+		for (let i = 0; i <= star_count - 1; i++) {
+			star_x[i] = randomGaussian(w/2, w/2);
+			star_y[i] = randomGaussian(h/2, h/2);
+			stars_made = true;
+		}
+	}
+
+	for (let i = 0; i <= star_count - 1; i++) {
+		fill(255);
+		circle(star_x[i], star_y[i], star_size);
+		star_y[i] += 0.1;
+		if (abs(randomGaussian(0, 3) > 6)) {
+		star_x[i] += randomGaussian(0, 1);
+		}
+		if (star_x[i] >= w || star_y[i] >= h) {
+			star_x[i] = randomGaussian(w/2, w/2);
+			star_y[i] = randomGaussian(h/2, h/2);
+		}
 	}
 }

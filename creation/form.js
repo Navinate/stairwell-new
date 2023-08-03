c3a2acfb8010af062d38c5f8b13a00d68bf40d5f
Maddy <59680197+xM4ddy@users.noreply.github.com>
2023-08-03 02:25:06 -0400

    Add Stuff

--- creation/form.js
+++ creation/form.js
@@ -1,5 +1,5 @@
-const socket = connectToWebSocket();
-console.log("connected to websocket");
+//const socket = connectToWebSocket();
+//console.log("connected to websocket");
 
 let maxPoints = 10;
 let noDraw = false;
@@ -8,52 +8,123 @@ let gest, gest2;
 let cnv;
 let hex;
 
-//setup of canvas and 2 gests
-function setup() {
-	cnv = createCanvas(800, 800);
-	frameRate(24);
+document.addEventListener("DOMContentLoaded", function(event) {
+	let pages = document.querySelectorAll(".page");
+	for (let i = 0; i < pages.length; i++) {
+		pages[i].style.display = "none";
+	}
+});
 
-	hex = document.querySelector('input[name="colors"]:checked').value;
-	gest = new Gesture(
-		color(hexToRgb(hex).r, hexToRgb(hex).g, hexToRgb(hex).b), // color
-		15, // agitatedness
-		0, // speed
-		0, // pointiness
-		255, // size
-	);
-	gest2 = new Gesture(
-		color(hexToRgb(hex).r, hexToRgb(hex).g, hexToRgb(hex).b), // color
-		15, // agitatedness
-		0, // speed
-		0, // pointiness
-		255, // size
-	);
-}
+// let goNext = document.querySelector(".go-next");
+// goNext.addEventListener("click", () => {
+// 	cyclePages();
+// 	updateGesture();
+// });
+// goNext.addEventListener("touchStart", () => {
+// 	cyclePages();
+// 	updateGesture();
+// });
 
-let index = 0;
+function cyclePages() {
+	if (true) {
+	//if (gest2.points.length > 0) {
+		pages.forEach((p) => {
+			p.style.display = "none";
+		});
+		pageIndex++;
 
-function draw() {
-	background(20);
-	if (!noDraw) {
-		// if (pmouseX < width && pmouseX > 0) {
-		// 	if (pmouseY < height && pmouseY > 0) {
-		// 		if (mouseIsPressed && gest2.points.length < maxPoints) {
-		// 			gest.addPoint(pmouseX, pmouseY);
-		// 			if (frameCount % 2 == 0) {
-		// 				gest2.addPoint(pmouseX, pmouseY);
-		// 				index++;
-		// 				if (index > maxPoints) {
-		// 					index = 0;
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// }
-		gest.render();
-	} else {
-		gest2.update();
+		pages[pageIndex].style.display = "flex";
+		if (pageIndex >= pages.length - 1) {
+			goNext.style.display = "none";
+		}
+
+		if (pageIndex == 8) {
+			document.querySelector(".go-next").style.display = "none";
+		} else {
+			//document.querySelector(".go-next").style.display = "block";
+		}
+
+		if (pageIndex == 9) {
+			document.querySelector(".p5Canvas").style.display = "none";
+		}
 	}
 }
+
+// var canvas = document.getElementsByClassName("p5Canvas");
+// document.body.addEventListener(
+//   "touchstart",
+//   function (e) {
+// 	if (e.target == canvas) {
+// 	  e.preventDefault();
+// 	}
+//   },
+//   { passive: false }
+// );
+// document.body.addEventListener(
+//   "touchend",
+//   function (e) {
+// 	if (e.target == canvas) {
+// 	  e.preventDefault();
+// 	}
+//   },
+//   { passive: false }
+// );
+// document.body.addEventListener(
+//   "touchmove",
+//   function (e) {
+// 	if (e.target == canvas) {
+// 	  e.preventDefault();
+// 	}
+//   },
+//   { passive: false }
+// );
+
+//setup of canvas and 2 gests
+// function setup() {
+// 	cnv = createCanvas(800, 800);
+// 	frameRate(24);
+
+// 	hex = document.querySelector('input[name="colors"]:checked').value;
+// 	gest = new Gesture(
+// 		color(hexToRgb(hex).r, hexToRgb(hex).g, hexToRgb(hex).b), // color
+// 		15, // agitatedness
+// 		0, // speed
+// 		0, // pointiness
+// 		255, // size
+// 	);
+// 	gest2 = new Gesture(
+// 		color(hexToRgb(hex).r, hexToRgb(hex).g, hexToRgb(hex).b), // color
+// 		15, // agitatedness
+// 		0, // speed
+// 		0, // pointiness
+// 		255, // size
+// 	);
+// }
+
+// let index = 0;
+
+// function draw() {
+// 	background(20);
+// 	if (!noDraw) {
+// 		if (pmouseX < width && pmouseX > 0) {
+// 			if (pmouseY < height && pmouseY > 0) {
+// 				if (mouseIsPressed && gest2.points.length < maxPoints) {
+// 					gest.addPoint(pmouseX, pmouseY);
+// 					if (frameCount % 2 == 0) {
+// 						gest2.addPoint(pmouseX, pmouseY);
+// 						index++;
+// 						if (index > maxPoints) {
+// 							index = 0;
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 		gest.render();
+// 	} else {
+// 		gest2.update();
+// 	}
+// }
 
 /* let submitGest = document.getElementById("submit-gest");
 submitGest.addEventListener("mousedown", () => {
@@ -102,100 +173,59 @@ function sendData() {
 //STORY LOGIC
 
 //randomly select a color and set grid backgrounds to color
-let radios = document.querySelectorAll('input[type="radio"]');
-radios[Math.floor(Math.random() * radios.length)].setAttribute("checked", "checked");
-radios.forEach((rb) => {
-	rb.style.backgroundColor = rb.value;
-});
+// let radios = document.querySelectorAll('input[type="radio"]');
+// radios[Math.floor(Math.random() * radios.length)].setAttribute("checked", "checked");
+// radios.forEach((rb) => {
+// 	rb.style.backgroundColor = rb.value;
+// });
 
-const caps = ["ROUND", "SQUARE", "PROJECT"];
-const joins = ["MITER", "BEVEL", "ROUND"];
+// const caps = ["ROUND", "SQUARE", "PROJECT"];
+// const joins = ["MITER", "BEVEL", "ROUND"];
 
-let pageIndex = 0;
+// let pageIndex = 0;
 
-//seed, hue, girth, cap, join, x, y, alpha, speed, wiggle, smoothness
-let seed = 0;
-let girth = 20;
-let cap = "ROUND";
-let join = "ROUND";
-let alphaVar = 255;
-let speed = 5;
-let wiggle = 250;
-let smoothness = 5;
+// //seed, hue, girth, cap, join, x, y, alpha, speed, wiggle, smoothness
+// let seed = 0;
+// let girth = 20;
+// let cap = "ROUND";
+// let join = "ROUND";
+// let alphaVar = 255;
+// let speed = 5;
+// let wiggle = 250;
+// let smoothness = 5;
 
-//Hide all but first page
-let pages = document.querySelectorAll(".page");
-for (let i = 1; i < pages.length; i++) {
-	pages[i].style.display = "none";
-}
+// let sendtoVisual = document.querySelector(".go-visual");
+// sendtoVisual.addEventListener("click", sendData);
+// sendtoVisual.addEventListener("touchStart", sendData);
 
-let goNext = document.querySelector(".go-next");
-goNext.addEventListener("click", () => {
-	cyclePages();
-	updateGesture();
-});
-goNext.addEventListener("touchStart", () => {
-	cyclePages();
-	updateGesture();
-});
+// function updateGesture() {
+// 	colorResult = hexToRgb(document.querySelector('input[type="radio"]:checked').value);
+// 	girth = map(document.querySelector("#a").value, 0, 100, 5, 150);
+// 	alphaVar = map(document.querySelector("#b").value, 0, 100, 128, 255);
+// 	speed = map(document.querySelector("#c").value, 0, 100, 1, 10);
+// 	wiggle = map(document.querySelector("#d").value, 0, 100, 10, 400);
+// 	smoothness = map(document.querySelector("#e").value, 0, 100, 1, 10);
+// 	//seed, hue, girth, cap, join, x, y, alpha, speed, wiggle, smoothness
+// 	gest2.color = color(colorResult.r, colorResult.g, colorResult.b, alphaVar);
+// 	gest2.girth = girth;
+// 	gest2.cap = cap;
+// 	gest2.join = join;
+// 	gest2.speed = speed;
+// 	gest2.wiggle = wiggle;
+// 	gest2.smoothness = smoothness;
+// 	if (!noDraw) {
+// 		//gest2.normalizePoints();
+// 	}
+// 	noDraw = true;
+// }
 
-let sendtoVisual = document.querySelector(".go-visual");
-sendtoVisual.addEventListener("click", sendData);
-sendtoVisual.addEventListener("touchStart", sendData);
-
-function cyclePages() {
-	if (true) {
-	//if (gest2.points.length > 0) {
-		pages.forEach((p) => {
-			p.style.display = "none";
-		});
-		pageIndex++;
-
-		pages[pageIndex].style.display = "flex";
-		if (pageIndex >= pages.length - 1) {
-			goNext.style.display = "none";
-		}
-
-		if (pageIndex == 8) {
-			document.querySelector(".go-next").style.display = "none";
-		} else {
-			//document.querySelector(".go-next").style.display = "block";
-		}
-
-		if (pageIndex == 9) {
-			document.querySelector(".p5Canvas").style.display = "none";
-		}
-	}
-}
-
-function updateGesture() {
-	colorResult = hexToRgb(document.querySelector('input[type="radio"]:checked').value);
-	girth = map(document.querySelector("#a").value, 0, 100, 5, 150);
-	alphaVar = map(document.querySelector("#b").value, 0, 100, 128, 255);
-	speed = map(document.querySelector("#c").value, 0, 100, 1, 10);
-	wiggle = map(document.querySelector("#d").value, 0, 100, 10, 400);
-	smoothness = map(document.querySelector("#e").value, 0, 100, 1, 10);
-	//seed, hue, girth, cap, join, x, y, alpha, speed, wiggle, smoothness
-	gest2.color = color(colorResult.r, colorResult.g, colorResult.b, alphaVar);
-	gest2.girth = girth;
-	gest2.cap = cap;
-	gest2.join = join;
-	gest2.speed = speed;
-	gest2.wiggle = wiggle;
-	gest2.smoothness = smoothness;
-	if (!noDraw) {
-		//gest2.normalizePoints();
-	}
-	noDraw = true;
-}
-
-function hexToRgb(hex) {
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result
-		? {
-				r: parseInt(result[1], 16),
-				g: parseInt(result[2], 16),
-				b: parseInt(result[3], 16),
-		  }
-		: null;
-}
+// function hexToRgb(hex) {
+// 	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+// 	return result
+// 		? {
+// 				r: parseInt(result[1], 16),
+// 				g: parseInt(result[2], 16),
+// 				b: parseInt(result[3], 16),
+// 		  }
+// 		: null;
+// }
--- form/welcome.js
+++ form/welcome.js
@@ -1,6 +1,3 @@
-var lastScrollTop = 0;
-var scroll_direction = 0; // 0 no scroll, 1 down, 2 up, 3 sideways
-
 //Hide all but first page
 let pages = document.querySelectorAll(".page");
 for (let i = 1; i < pages.length; i++) {
@@ -30,23 +27,3 @@ function cyclePages() {
 	}
 }
 
-
-
-document.addEventListener("scroll", function () {
-	var st = window.scrollY || document.documentElement.scrollTop;
-
-	if (st > lastScrollTop) { // scrolling down
-		console.log("scrolling down");
-		scroll_direction = 1;
-	}
-	else if (st < lastScrollTop) { // scrolling up
-		console.log("scrolling up");
-		scroll_direction = 2;
-	}
-	else { // scrolling horizontal
-		console.log("scrolling sizeways");
-		scroll_direction = 3;
-	}
-
-	lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
